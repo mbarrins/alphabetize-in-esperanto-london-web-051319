8da0622cc589db41cb1dd95403fce2ff03e52f5e
Maija Barrins <maija.barrins@gmail.com>
2019-05-01 12:33:12 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,12 +1,9 @@
 require "pry"
 
 def alphabetize(arr)
-  binding.pry
-  # ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   # binding.pry
-  # arr.sort_by{|str| str.map}
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.map{|str| str.chars.inject([] {|result, char| result << (ESPERANTO_ALPHABET.index(char) || char)}).join("")}
+  arr.sort_by{|str| str.chars.inject([]) {|result, char| result << (ESPERANTO_ALPHABET.index(char) || char)}}
 end
 
 arr = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
